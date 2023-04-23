e89d1a9cd794a5700d04b1eef1c1e1c581ae0bbb
Mike Esposito <mikeesposito6852@gmail.com>
2023-04-23 10:43:01 -0700

    Add invitees.js

---
+++ test/invitees.js
@@ -0,0 +1,19 @@
+const {Browser, By, Key, until} = require("selenium-webdriver");
+const {suite} = require("selenium-webdriver/testing");
+const assert = require("assert");
+
+const url = "https://treehouse-projects.github.io/selenium-webdriver-intermediate/waits/app/index.html";
+
+suite(function(env) {
+  describe('RSVP site', function() {
+    it('has invitee list', function() {
+      env.builder().build()
+        .then(driver => {
+          driver.get(url)
+            .then(() => driver.findElements(By.id('invitedList')))
+            .then(elements => assert(elements.length > 0))
+            .then(() => driver.quit());
+        });
+    });
+  });
+});
