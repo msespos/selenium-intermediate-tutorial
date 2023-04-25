0e5f050d6e7bc554d286a69f04d0bf02d04f4aca
Mike Esposito <mikeesposito6852@gmail.com>
2023-04-25 12:04:20 -0700

    Add test for scrolling

---
+++ test/scroll.js
@@ -0,0 +1,33 @@
+const {Browser, By, Key, until} = require("selenium-webdriver");
+const {suite} = require("selenium-webdriver/testing");
+const assert = require("assert");
+const ScrollPage = require('../pages/scroll.js');
+
+suite(function(env) {
+  describe('Infinite scroll demo', function() {
+    let driver;
+    let page;
+
+    before(async function() {
+      this.timeout(10000);
+      driver = await env.builder().build();
+      page = new ScrollPage(driver);
+      await page.open();
+    });
+
+    it('adds more boxes', async function() {
+      let boxes = await driver.findElements(page.locators.boxes);
+      let oldBoxCount = boxes.length;
+      console.log(oldBoxCount);
+      await page.loadContent();
+      boxes = await driver.findElements(page.locators.boxes);
+      let newBoxCount = boxes.length;
+      console.log(newBoxCount);
+      assert(newBoxCount > oldBoxCount)
+    });
+
+    after(async function() {
+      //driver.quit();
+    });
+  });
+});
