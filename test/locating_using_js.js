cbe687a93b4adb1ea83a8fe56715eb93a93c00aa
Mike Esposito <mikeesposito6852@gmail.com>
2023-04-25 15:35:58 -0700

    Add tests for locating using JS

---
+++ test/locating_using_js.js
@@ -0,0 +1,29 @@
+const {Browser, By, Key, until} = require("selenium-webdriver");
+const {suite} = require("selenium-webdriver/testing");
+const assert = require("assert");
+const ButtonsPage = require('../pages/locating_using_js.js');
+
+suite(function(env) {
+  describe('Save button', function() {
+    let driver;
+    let page;
+
+    before(async function() {
+      this.timeout(10000);
+      driver = await env.builder().build();
+      page = new ButtonsPage(driver);
+      await page.open();
+    });
+
+    it('Updates status text', async function() {
+      await page.clickSave();
+      let span = await driver.findElement(page.locators.status);
+      let text = await span.getText();
+      assert(text.includes("Saved!"));
+    });
+
+    after(async function() {
+      driver.quit();
+    });
+  });
+});
